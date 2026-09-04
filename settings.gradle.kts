7dabcaaff28a5cf65478e8742ce6f13aff1303a6
S.S. <safijasamsudinova@gmail.com>
2026-09-04 20:47:00 +0200

    Add Sherpa-ONNX Maven repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://k2-fsa.github.io/sherpa/maven-repo/") }
     }
 }
 
